a9a58ecb4334857ae82594b95f3fe401e590725a
Ivan Trusenev <ivantrusenev@gmail.com>
2026-09-26 01:30:25 +0300

    Initial commit: AudioNap Xcode project skeleton

---
+++ AudioNapTests/AudioNapTests.swift
@@ -0,0 +1,18 @@
+//
+//  AudioNapTests.swift
+//  AudioNapTests
+//
+//  Created by Ivan Trusenev on 26.09.2026.
+//
+
+import Testing
+
+struct AudioNapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ audionapd/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  audionapd
+//
+//  Created by Ivan Trusenev on 26.09.2026.
+//
+
+import Foundation
+
+print("Hello, World!")
+
